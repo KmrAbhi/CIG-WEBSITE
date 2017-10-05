28ed1df764259dafe907e4493893da776a6a8739
KmrAbhi <abhishekraj6040@gmail.com>
2017-10-06 01:40:35 +0530

    header ready and basic home layout

---
+++ header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+<body>
+<div class="header">
+<div class="header-images">
+<div class="header-logo"><img src="images/logo.jpg"></div>
+<div class="header-title"><img src="images/title.jpg">
+<div class="nav"><ul class ="nav-objects">
+<li><a href="#">Home</a></li>
+<li><a href="#">Industries</a></li>
+<li><a href="#">Allies</a></li>
+<li><a href="#">Allies</a></li>
+<li><a href="#">Allies</a></li>
+<li><a href="#">Allies</a></li>
+<li><a href="#">Allies</a></li>
+<li><a href="#">Allies</a></li>
+</ul></div></div>
+</div>
+</div>
+</body>
+</html>
