fb5cce7734faf918a015df7b9f577f1003f4ebb9
KmrAbhi <abhishekraj6040@gmail.com>
2017-10-06 15:28:32 +0530

    minor final adjustments

--- home.php
+++ home.php
@@ -1,17 +1,16 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<!-- css -->
- 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body background="images/background.jpg">
-	<!--header -->
+	
 	<?php include 'header.php';?>
 <div class="home-inner-body">
 <div class="one-third left"><a>aba</a></div>
 <div class="one-third center"><a>a</a></div>
 <div class="one-third right"><a>abc</a></div>
-	</div>
+</div>
 	<?php include 'footer.php';?>
 
 </body>
